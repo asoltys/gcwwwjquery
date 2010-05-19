326deeea29d418733c9b25f2c45b628a6aa8f7b0
pjackson <pjackson@386b6a11-e5be-4a38-a82d-8bea4b33e717>
2010-05-19 03:50:52 +0000

    git-svn-id: https://svn.ircan.gc.ca/svn/gcwwwjquery@6 386b6a11-e5be-4a38-a82d-8bea4b33e717

---
+++ js/lib/goc.utils.js
@@ -0,0 +1,59 @@
+//parseUri 1.2.2
+// (c) Steven Levithan <stevenlevithan.com>
+// MIT License
+
+function parseUri (str) {
+	var	o   = parseUri.options,
+		m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+		uri = {},
+		i   = 14;
+
+	while (i--) uri[o.key[i]] = m[i] || "";
+
+	uri[o.q.name] = {};
+	uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+		if ($1) uri[o.q.name][$1] = $2;
+	});
+
+	return uri;
+};
+
+parseUri.options = {
+	strictMode: false,
+	key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	q:   {
+		name:   "queryKey",
+		parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	},
+	parser: {
+		strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+		loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	}
+};
+
+/** 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * **/
+
+var Utils = {
+		getLibraryPath : function() { return PE.liblocation + "lib"; },
+		getSupportPath : function() { return PE.liblocation + "support"; },
+        getPluginsPath : function() { return PE.liblocation + "plugins"; },
+	    loadParamsFromScriptID : function(name) {  
+        	var parameters = parseUri(jQuery("script[id='goc_plugin_" + name + "']").attr('src')).queryKey;
+             return parameters;
+        		},
+        addCSSSupportFile : function(pathtofile) { document.write('<link rel="stylesheet" href="'+pathtofile+'" type="text/css" media="screen" />');  },
+        
+        addKeyboardBindingToPlugin : function(objec, keyboardaction, keyboardsequence, func) {
+        	objec.focus(function(){
+        		objec.bind(keyboardaction, keyboardsequence, func);
+        		});
+        },
+        getPageLanguage : function() {return jQuery("meta[name='dc.language']").attr('content'); }
+};
