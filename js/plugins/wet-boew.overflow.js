40c7afec62c9b12376e98cdb45e17e2d6a05c43f
pjackson <pjackson@386b6a11-e5be-4a38-a82d-8bea4b33e717>
2010-07-01 02:15:26 +0000

    # Re-added wet-boew.overflow.js which was previously removed from the repository by accident.
    git-svn-id: https://svn.ircan.gc.ca/svn/gcwwwjquery@23 386b6a11-e5be-4a38-a82d-8bea4b33e717

---
+++ js/plugins/wet-boew.overflow.js
@@ -0,0 +1,36 @@
+/* WEB EXPERIENCE TOOLKIT (WET) | BOITE A OUTILS DE L'EXPERIENCE WEB (BOEW) 
+TERMS AND CONDITIONS OF USE: http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Terms
+CONDITIONS REGISSANT L'UTILISATION : http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Conditions
+*/
+
+/**
+*    ieOverflow - To allow for IE 5.5 + 6 + to act like a normal standards compliant browser, allowing overflow in parent containers
+*    		     flow outside the box, instead of breaking the page.
+**/
+var ieOverflow = {
+	params :  Utils.loadParamsFromScriptID("overflow"),
+	init: function() {
+
+		/** Check to see what class to call for the functionality **/
+	    this.params.id = (this.params.id == "default"  ) ? ".ieoverflow" : "." + this.params.id;
+        /** End of parameter qualifying **/
+	    
+	if ( /MSIE ((5\.5)|6)/.test(navigator.userAgent) && navigator.platform == "Win32" ) {	
+		$("div.center").find(params.id.toString()).each(function() {
+	 		var hght = Math.round((jQuery(this).height()/16)*Math.pow(10,1))/Math.pow(10,1);
+			var cssH = hght + "em";
+			var stylet = 'hieght:'+cssH+';width:98.5%';
+			jQuery(this).wrap($("<div></div>").css("height",cssH).css("width","98.5%"));
+			jQuery(this).css('position','absolute');
+			jQuery(this).parent().after('<br /><br /><br /><br /><br />');
+
+			}
+		);};
+	}
+};
+
+/**
+ *  ieOverFlow Runtime
+ **/
+
+$("document").ready(function(){   ieOverflow.init(); });
