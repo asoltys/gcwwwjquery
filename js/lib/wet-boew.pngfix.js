ff3134d0a194859dadd52c1266fea014375e333b
pjackson <pjackson@386b6a11-e5be-4a38-a82d-8bea4b33e717>
2010-08-06 05:24:17 +0000

    # Corrected overflow fix so only affects the root causes of the columns stretching # Corrected height calculation of overflow fix
    git-svn-id: https://svn.ircan.gc.ca/svn/gcwwwjquery@42 386b6a11-e5be-4a38-a82d-8bea4b33e717

--- js/lib/wet-boew.pngfix.js
+++ js/lib/wet-boew.pngfix.js
@@ -42,47 +42,50 @@ var ieOptimzier = {
 
 var overFlowFix = {
 	
-	maxLeftWidth : 0,
+	maxLeftWidth: 0,
 	maxCentreWidth: 0,
 	maxRightWidth: 0,
 	
 	stabilize: function() {
 		
-		 // Figure out the maxWidths for each of the columns
-		 if ($('#cn-centre-col').length > 0) {
-			this.maxCentreWidth = $('#cn-centre-col-gap').width();
-		 };
-		 if ($('#cn-left-col').length > 0) {
-		 		this.maxLeftWidth = $('#cn-left-col-gap').width();
-		 };
+		// Figure out the maxWidths for each of the columns
+		if ($('#cn-body-inner-1col').length > 0) this.maxCentreWidth = $('#cn-centre-col').width();
+		else this.maxCentreWidth = $('#cn-centre-col-gap').width() - ($('#cn-centre-col-inner').outerWidth() - $('#cn-centre-col-inner').width());
+		if ($('#cn-left-col').length > 0) this.maxLeftWidth = $('#cn-left-col-gap').width();
+		if ($('#cn-right-col').length > 0) this.maxRightWidth = $('#cn-right-col-gap').width();
 		
-		if ($('#cn-right-col').length > 0) {
-			this.maxRightWidth = $('#cn-right-col-gap').width();
-		   	}
-		
-		// Check to see if the left column has been stretched. If so, call fixOverflow to fix it.
-		if ( this.maxLeftWidth > 0 && $('#cn-left-col').width() > this.maxLeftWidth) {
-		   this.adjust("#cn-left-col", this.maxLeftWidth);
-		   // Remove overflow-x: hidden since issue has been fixed
-		   $('#cn-left-col').css('overflow-x','visible');
+		// Fix left column if it has been stretched
+		if ( this.maxLeftWidth > 0 && $('#cn-left-col').outerWidth() > this.maxLeftWidth) {
+			this.adjust($('#cn-left-col'), this.maxLeftWidth);
+			$('#cn-left-col').css('overflow-x','visible');
 		}		
-		// Check to see if the centre column has been stretched. If so, call fixOverflow to fix it.
-		if ($('#cn-centre-col-inner').innerWidth() > $('#cn-centre-col-gap').width()) {
-		   this.adjust("#cn-centre-col-inner", this.maxCentreWidth);
-		   // Remove overflow-x: hidden since issue has been fixed
-		   $('#cn-centre-col').css('overflow-x','visible');
+		// Fix centre column if it has been stretched
+		if ($('#cn-centre-col-inner').outerWidth() > $('#cn-centre-col-gap').width()) {
+			this.adjust($('#cn-centre-col-inner'), this.maxCentreWidth);
+			$('#cn-centre-col').css('overflow-x','visible');
 		}
-		// Check to see if the right column has been stretched. If so, call fixOverflow to fix it.
-		if (this.maxRightWidth > 0 && $('#cn-right-col').width() > this.maxRightWidth) {
-			this.adjust("#cn-right-col", this.maxRightWidth);
-		   // Remove overflow-x: hidden since issue has been fixed
-		   $('#cn-right-col').css('overflow-x','visible');
+		// Fix right column if it has been stretched
+		if (this.maxRightWidth > 0 && $('#cn-right-col').outerWidth() > this.maxRightWidth) {
+			this.adjust($('#cn-right-col'), this.maxRightWidth);
+			$('#cn-right-col').css('overflow-x','visible');
 		}
 	},
 	adjust: function(container, maxWidth) {
-		   // Find each top-level element in container that is wider than maxWidth and do the following
-		   $(container).css('position', 'absolute').wrap('<div style="width: 100%; height: ' + (Math.round(($(container).height()/16)*Math.pow(10,1))/Math.pow(10,1)) + 'em;"></div>');	   
+		var fixesNeeded = false;
+		var actualHeight = 0;
+		
+		// Allow each top-level element that is wider than maxWidth to overflow the container
+		container.children().each(function() {
+			if ($(this).innerWidth() > maxWidth) {
+				if (!overFlowFix.adjust($(this), maxWidth)) {
+					actualHeight = (Math.round((($(this).outerHeight(true))/16)*Math.pow(10,1))/Math.pow(10,1));
+					$(this).css('position', 'absolute').wrap('<div style="width: ' + (maxWidth - 8) + 'px; height: ' + actualHeight + 'em;"></div>');
+				}
+				fixesNeeded = true;
 			}
+		});
+		return fixesNeeded;
+	}
 }
 /**
  *  PngFix Runtime
@@ -93,10 +96,8 @@ $("document").ready(function(){
 	PngFix.init(); 	
 	
 	if(PngFix.isOlderIE) {
-		
 	  PngFix.helpIe();
 	  overFlowFix.stabilize();
 	  ieOptimzier.optimize();
-	
 	}
 });
