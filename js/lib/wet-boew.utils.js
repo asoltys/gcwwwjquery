1c29e2c9d285090c76a7a173649fe3660aa6265c
pjackson <pjackson@386b6a11-e5be-4a38-a82d-8bea4b33e717>
2011-02-04 01:40:05 +0000

    # Changed media="screen" to media="all" for addCSSSupportFile function
    git-svn-id: https://svn.ircan.gc.ca/svn/gcwwwjquery@79 386b6a11-e5be-4a38-a82d-8bea4b33e717

--- js/lib/wet-boew.utils.js
+++ js/lib/wet-boew.utils.js
@@ -71,14 +71,14 @@ var Utils =
     // Removed the href attribution in the original link creation to address warnings trigger by IE 8
     addCSSSupportFile: function (pathtofile)
     {
-        var $link = jQuery('<link rel="stylesheet" type="text/css" media="screen" />').appendTo('head');
+        var $link = jQuery('<link rel="stylesheet" type="text/css" media="all" />').appendTo('head');
         // See http://www.subchild.com/2010/05/20/cross-browser-problem-with-dynamic-css-loading-with-jquery-1-4-solved/
         $link.attr(
         {
             href: pathtofile,
             rel: 'stylesheet',
             type: 'text/css',
-            media: 'screen'
+            media: 'all'
         });
 
     },
@@ -94,7 +94,7 @@ var Utils =
 	    }
 	    
 	    if (condition != ""){
-		    document.write('<!--[if ' + condition + ']><link rel="stylesheet" href="' + pathtofile + '" type="text/css" media="screen" /><![endif]-->');
+		    document.write('<!--[if ' + condition + ']><link rel="stylesheet" href="' + pathtofile + '" type="text/css" media="all" /><![endif]-->');
 	    }
     },
     addKeyboardBindingToPlugin: function (objec, keyboardaction, keyboardsequence, func)
