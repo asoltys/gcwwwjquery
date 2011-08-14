39f9311d49b95e8fba0ab09dfe73434c0b013ebb
pjackson <pjackson@386b6a11-e5be-4a38-a82d-8bea4b33e717>
2011-08-14 05:15:29 +0000

    # Updated in source version numbers
    git-svn-id: https://svn.ircan.gc.ca/svn/gcwwwjquery@95 386b6a11-e5be-4a38-a82d-8bea4b33e717

--- js/pe-ap.js
+++ js/pe-ap.js
@@ -1,5 +1,5 @@
 ﻿/*!
- * jQuery integration v1.2 / Intégration jQuery v1.2
+ * jQuery integration v1.3a2 / Intégration jQuery v1.3a2
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * Terms and conditions of use: http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Terms
  * Conditions régissant l'utilisation : http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Conditions
