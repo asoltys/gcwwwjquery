c6f2a4d0e4535625d297fd8fcd57bedb66d65693
pjackson <pjackson@386b6a11-e5be-4a38-a82d-8bea4b33e717>
2011-12-11 06:11:18 +0000

    # Updated version numbers of original WET framework to v1.3b1
    git-svn-id: https://svn.ircan.gc.ca/svn/gcwwwjquery@119 386b6a11-e5be-4a38-a82d-8bea4b33e717

--- branches/jquery-build/js/lib/wet-boew.utils.js
+++ branches/jquery-build/js/lib/wet-boew.utils.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery integration v1.3a7 / Intégration jQuery v1.3a7
+ * jQuery integration v1.3b1 / Intégration jQuery v1.3b1
  * Web Experience Toolkit (WET) / Boîte à outils de l'expérience Web (BOEW)
  * Terms and conditions of use: http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Terms
  * Conditions régissant l'utilisation : http://tbs-sct.ircan.gc.ca/projects/gcwwwtemplates/wiki/Conditions
